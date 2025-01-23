c42ce37e44ec9e9da6f775ef5c663eb0f35af436
SoeurnRotha_Blue <rotha_soeurn@blue.com.kh>
2025-01-23 09:28:46 +0700

    photo manager

---
+++ lib/core/service/photo_manager_service.dart
@@ -0,0 +1,48 @@
+import 'package:photo_manager/photo_manager.dart';
+
+class PhotoManagerService {
+  static final PhotoManagerService instance = PhotoManagerService._internal();
+  PhotoManagerService._internal();
+
+  Future<bool> checkPermissions() async {
+    // the method can use optional param `permission`.
+    final PermissionState ps = await PhotoManager.requestPermissionExtend();
+    if (ps.isAuth) {
+      // Granted
+      // You can to get assets here.
+      return true;
+    } else if (ps.hasAccess) {
+      // Access will continue, but the amount visible depends on the user's selection.
+      return true;
+    } else {
+      // Limited(iOS) or Rejected, use `==` for more precise judgements.
+      // You can call `PhotoManager.openSetting()` to open settings for further steps.
+      PhotoManager.openSetting();
+      return false;
+    }
+  }
+
+  /// Fetch albums (folders) and their assets
+  Future<List<AssetPathEntity>> getAlbums() async {
+    // Fetching albums (folders) from the gallery
+    final List<AssetPathEntity> albums = await PhotoManager.getAssetPathList(
+      type: RequestType.image, // You can specify image, video, or audio
+      filterOption: FilterOptionGroup(
+        containsPathModified: true,
+        orders: [OrderOption(type: OrderOptionType.createDate, asc: false)],
+      ),
+    );
+    return albums;
+  }
+
+  /// Fetch assets (photos/videos) from a specific album
+  Future<List<AssetEntity>> getAlbumsFolders() async {
+    final int count = await PhotoManager.getAssetCount();
+    final entities = await PhotoManager.getAssetListPaged(
+      page: 0,
+      pageCount: count,
+      type: RequestType.image,
+    );
+    return entities;
+  }
+}
