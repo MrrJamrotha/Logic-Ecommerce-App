bcba6e471f8edbb33ecd49be18146f2a284dcee9
SoeurnRotha_Blue <rotha_soeurn@blue.com.kh>
2025-01-31 15:28:01 +0700

    f

--- lib/presentation/global/application/application_cubit.dart
+++ lib/presentation/global/application/application_cubit.dart
@@ -1,27 +1,24 @@
 import 'package:hydrated_bloc/hydrated_bloc.dart';
-import 'package:logic_app/core/constants/app_shared_preference_keys.dart';
-import 'package:logic_app/core/di/injection.dart';
 import 'package:logic_app/core/helper/helper.dart';
-import 'package:logic_app/core/service/shared_preferences_service.dart';
 import 'package:logic_app/presentation/global/application/application_state.dart';
 
 class ApplicationCubit extends HydratedCubit<ApplicationState> {
   ApplicationCubit() : super(ApplicationState());
-  final _sharedPreferences = di.get<SharedPreferencesService>();
+  // final _sharedPreferences = di.get<SharedPreferencesService>();
 
   Future<void> loadInitialData() async {
-    try {
-      final result = await _sharedPreferences.getBool(onBoradingKey) ?? false;
-      emit(state.copyWith(onBoarding: result));
-    } catch (e) {
-      emit(state.copyWith(onBoarding: false));
-      addError(e);
-    }
+    // try {
+    //   final result = await _sharedPreferences.getBool(onBoradingKey) ?? false;
+    //   emit(state.copyWith(onBoarding: result));
+    // } catch (e) {
+    //   emit(state.copyWith(onBoarding: false));
+    //   addError(e);
+    // }
   }
 
   Future<void> setOnboarding(bool value) async {
     try {
-      await _sharedPreferences.setBool(onBoradingKey, value);
+      // await _sharedPreferences.setBool(onBoradingKey, value);
       emit(state.copyWith(onBoarding: value));
     } catch (e) {
       addError(e);
