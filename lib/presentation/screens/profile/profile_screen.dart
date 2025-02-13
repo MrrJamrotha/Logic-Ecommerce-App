da916e4337eadb27f92f21d522ca00abd9d4bf9f
MrrJamrotha <mrrjamrotha@gmail.com>
2025-02-13 12:16:03 +0700

    fixed

--- lib/presentation/screens/profile/profile_screen.dart
+++ lib/presentation/screens/profile/profile_screen.dart
@@ -4,6 +4,7 @@ import 'package:logic_app/core/helper/helper.dart';
 import 'package:logic_app/presentation/screens/profile/profile_cubit.dart';
 import 'package:logic_app/presentation/screens/profile/profile_state.dart';
 import 'package:logic_app/presentation/widgets/app_bar_widget.dart';
+import 'package:logic_app/presentation/widgets/catch_image_network_widget.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({Key? key}) : super(key: key);
@@ -44,10 +45,24 @@ class _ProfileScreenState extends State<ProfileScreen> {
   }
 
   Widget buildBody(ProfileState state) {
-    return ListView(
-      children: [
-        // TODO your code here
-      ],
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Row(
+            children: [
+              CatchImageNetworkWidget(
+                width: 120.scale,
+                height: 120.scale,
+                boxFit: BoxFit.cover,
+                borderRadius: BorderRadius.circular(100.scale),
+                imageUrl:
+                    'https://sm.ign.com/t/ign_nordic/feature/t/the-avatar/the-avatar-the-last-airbender-trailer-gets-a-lot-right-but-w_tkwa.1200.jpg',
+                blurHash: 'LEHV6nWB2yk8pyo0adR*.7kCMdnj',
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
